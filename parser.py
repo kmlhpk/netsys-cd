90c41b7b8ed72efc19e8e80df44f2f68015b2b60
kmlhpk <44464908+kmlhpk@users.noreply.github.com>
2020-03-03 21:38:34 +0000

    progress on file ingestion

--- parser.py
+++ parser.py
@@ -1,12 +1,12 @@
 import sys
 import pathlib
+import re
 
 if len(sys.argv) < 2:
     print("ERROR: Please run the program including your filename as a parameter, in the format of 'python parser.py filename.ext'")
     sys.exit()
 
 pathname = sys.argv[1]
-
 path = pathlib.Path.cwd() / pathname
 
 with open(path,"r") as f:
@@ -14,14 +14,51 @@ with open(path,"r") as f:
     for line in f:
         content.append(line)
 
-print(content)
+for i in range(len(content)):
+    content[i] = content[i].replace("\n","")
 
+equality = set()
+connectives = set()
+quantifiers = set()
+variables = set()
+constants = set()
+predicateList = []
+predicates = {}
 
-#content = content.replace("\n","")
-#print(content)
-#content = content.replace("\t","")
-#print(content)
-#content = content.replace(" ","")
-#print(content)
-#content = content.replace(";;",";")
-#print(content)
+toPop = []
+
+for i in range(len(content)):
+    if content[i][0:10] == "variables:":
+        variables = set(content[i][11:].split(" "))
+        toPop.append(i)
+    elif content[i][0:10] == "constants:":
+        constants = set(content[i][11:].split(" "))
+        toPop.append(i)
+    elif content[i][0:11] == "predicates:":
+        predicateList = content[i][12:].split(" ")
+        for i in predicateList:
+
+        toPop.append(i)
+    elif content[i][0:9] == "equality:":
+        equality = set(content[i][10:].split(" "))
+        toPop.append(i)
+    elif content[i][0:12] == "connectives:":
+        connectives = set(content[i][13:].split(" "))
+        toPop.append(i)
+    elif content[i][0:12] == "quantifiers:":
+        quantifiers = set(content[i][13:].split(" "))
+        toPop.append(i)
+
+toPop.sort(reverse=True)
+for i in toPop:
+    content.pop(i)
+
+formula = "".join(content)[9:]
+
+print(equality,connectives,quantifiers,predicates,variables,constants)
+print(formula)
+
+while "  " in formula:
+    formula = formula.replace("  "," ")
+
+print(formula)
